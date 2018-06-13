bdc60eceb354d01a4ca7c7cd0635ae1b0f45bae0
Oleg Ryasnoy <ryasnoy.oleg@gmail.com>
2018-06-13 15:15:51 +0300

    Config in progress

--- app.js
+++ app.js
@@ -2,7 +2,39 @@ const TOKEN = require('./token.js')
 const TelegramBot = require('node-telegram-bot-api')
 const bot = new TelegramBot(TOKEN, { polling: true })
 
-bot.on('message', msg => {
+var dateOptions = {
+    reply_markup: JSON.stringify({
+        inline_keyboard: [
+            [{ text: 'Today', callback_data: '0' }],
+            [{ text: 'Yesterday', callback_data: '1' }],
+            [{ text: 'Custom Date', callback_data: '2' }]
+        ]
+    })
+};
 
-    bot.sendMessage(msg.chat.id, "Hello World!")
+bot.onText(/\/all/, function (msg, match) {
+    
+});
+
+bot.onText(/\/worker/, function (msg, match) {
+    bot.sendMessage(msg.chat.id, 'Enter your last name:');
+    // TODO: CHECK AND SELECT USER FROM DATABASE
+    //bot.sendMessage(msg.chat.id, 'Set the time period:', dateOptions);
+});
+
+bot.on('callback_query', function (msg) {
+    // console.log(msg)
+
+    switch (parseInt(msg.data)) {
+        case 0:
+            bot.sendMessage(msg.from.id, '0')
+            break
+        case 1: bot.sendMessage(msg.from.id, '1')
+            break
+        case 2: bot.sendMessage(msg.from.id, '2')
+            break
+        default:
+            bot.sendMessage(msg.from.id, "undefined")
+            break
+    }
 })
